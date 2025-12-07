514a80460e1ca978c30a6065e97d803bb7aa2100
Praveen Kithani <praveenk.kithani@gmail.com>
2025-12-07 15:06:17 +0530

    Initial commit

---
+++ src/main/java/com/praveen/kafka/example/kafka_producer_products_service/KafkaProducerProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.kafka.example.kafka_producer_products_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerProductsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/kafka/example/kafka_producer_products_service/KafkaProducerProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.kafka.example.kafka_producer_products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
